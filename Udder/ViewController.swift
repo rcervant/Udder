a01ae530166c5b78f5797d6e83554b11729cfe26
Ritchie Cervantes <ritchie.cervantes@gmail.com>
2019-05-08 13:43:07 -0500

    add miltank and labels

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Ritchie Cervantes on 5/8/19.
+//  Copyright © 2019 Ritchie Cervantes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
