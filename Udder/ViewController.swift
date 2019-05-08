3f20a618a2914a4f64a69605f231321d3c11a5b6
Ritchie Cervantes <ritchie.cervantes@gmail.com>
2019-05-08 15:00:36 -0500

    add performSegue

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func performSegue(withIdentifier identifier: String, sender: Any?) {
+        //
+    }
 
 
 }
